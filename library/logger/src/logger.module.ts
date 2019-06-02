bf3f1faf9b83320fb8b7bd350a0a0e42c4772f9f
codemile <nick@reactgular.com>
2019-06-02 10:09:40 -0400

    added CommonModule

--- library/logger/src/logger.module.ts
+++ library/logger/src/logger.module.ts
@@ -1,3 +1,4 @@
+import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {NgtUniversalModule} from '@ng-toolkit/universal';
 import {LoggerService} from './logger/loggerService';
@@ -5,6 +6,7 @@ import {StackTraceService} from './stack-trace/stack-trace.service';
 
 @NgModule({
     imports: [
+        CommonModule,
         NgtUniversalModule
     ],
     providers: [
