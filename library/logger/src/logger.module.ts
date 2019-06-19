f812db2a2577548471d8146500a1693b58a380ff
Nick Foscarini <nick@reactgular.com>
2019-06-19 08:14:01 -0400

    fixes for root

--- library/logger/src/logger.module.ts
+++ library/logger/src/logger.module.ts
@@ -1,24 +1,21 @@
-import {CommonModule} from '@angular/common';
-import {NgModule, Provider} from '@angular/core';
+import {ModuleWithProviders, NgModule, Provider} from '@angular/core';
 import {LogNoopService} from './log-noop/log-noop.service';
 import {LogService} from './log/log.service';
 import {LOGGER_ALL, LOGGER_CONSOLE, LOGGER_LEVELS, LOGGER_TAILS, LOGGER_TAILS_DEFAULT, LoggerConfig} from './logger-types';
 import {LogConsoleService} from './log-console/log-console.service';
 
-@NgModule({})
+@NgModule()
 export class LoggerModule {
     /**
      * Call this method to import the logger module into the app module.
      */
-    public static forRoot(options?: LoggerConfig): NgModule {
+    public static forRoot(options?: LoggerConfig): ModuleWithProviders {
         options = Object.assign({
             enabled: true,
             levels: LOGGER_ALL,
             tails: LOGGER_TAILS_DEFAULT,
             console: console
         } as LoggerConfig, options || {});
-
-        const imports = [CommonModule];
 
         const providers: Provider[] = [
             {provide: LOGGER_LEVELS, useValue: options.levels},
@@ -32,6 +29,6 @@ export class LoggerModule {
             providers.push({provide: LogService, useClass: LogNoopService});
         }
 
-        return {imports, providers};
+        return {ngModule: LoggerModule, providers};
     }
 }
