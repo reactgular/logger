94c53edb2b07ccfb629426e3196d05c96b4ddb3c
codemile <nick@reactgular.com>
2019-12-03 22:31:11 -0500

    fixes functions error when building with newer versions of Angular and AOT

--- library/logger/src/logger.module.ts
+++ library/logger/src/logger.module.ts
@@ -1,34 +1,52 @@
-import {ModuleWithProviders, NgModule, Provider} from '@angular/core';
+import {InjectionToken, ModuleWithProviders, NgModule} from '@angular/core';
+import {LogConsoleService} from './log-console/log-console.service';
 import {LogNoopService} from './log-noop/log-noop.service';
 import {LogService} from './log/log.service';
-import {LOGGER_ALL, LOGGER_CONSOLE, LOGGER_LEVELS, LOGGER_TAILS, LOGGER_TAILS_DEFAULT, LoggerConfig} from './logger-types';
-import {LogConsoleService} from './log-console/log-console.service';
+import {
+    ConsoleMethods,
+    LOGGER_ALL,
+    LOGGER_CONSOLE,
+    LOGGER_LEVEL,
+    LOGGER_LEVELS,
+    LOGGER_TAILS,
+    LOGGER_TAILS_DEFAULT,
+    LoggerConfig
+} from './logger-types';
+import {PrefixService} from './prefix/prefix.service';
 
-@NgModule()
+export const LOGGER_OPTIONS: InjectionToken<LoggerConfig> = new InjectionToken<LoggerConfig>('LOGGER_OPTIONS');
+
+export function LogServiceFactory(
+    levels: LOGGER_LEVEL,
+    console: ConsoleMethods<void>,
+    prefixService: PrefixService,
+    loggerConfig: LoggerConfig
+) {
+    return loggerConfig && loggerConfig.enabled
+        ? new LogConsoleService(levels, console, prefixService)
+        : new LogNoopService();
+}
+
+// @dynamic
+@NgModule({})
 export class LoggerModule {
     /**
      * Call this method to import the logger module into the app module.
      */
-    public static forRoot(options?: LoggerConfig): ModuleWithProviders {
-        options = Object.assign({
-            enabled: true,
-            levels: LOGGER_ALL,
-            tails: LOGGER_TAILS_DEFAULT,
-            console: console
-        } as LoggerConfig, options || {});
-
-        const providers: Provider[] = [
-            {provide: LOGGER_LEVELS, useValue: options.levels},
-            {provide: LOGGER_TAILS, useValue: options.tails},
-            {provide: LOGGER_CONSOLE, useValue: options.console}
-        ];
-
-        if (options && options.enabled) {
-            providers.push({provide: LogService, useClass: LogConsoleService});
-        } else {
-            providers.push({provide: LogService, useClass: LogNoopService});
-        }
-
-        return {ngModule: LoggerModule, providers};
+    public static forRoot(loggerConfig?: LoggerConfig): ModuleWithProviders<LoggerModule> {
+        return {
+            ngModule: LoggerModule,
+            providers: [
+                {provide: LOGGER_LEVELS, useValue: loggerConfig.levels || LOGGER_ALL},
+                {provide: LOGGER_TAILS, useValue: loggerConfig.tails || LOGGER_TAILS_DEFAULT},
+                {provide: LOGGER_CONSOLE, useValue: loggerConfig.console || console},
+                {provide: LOGGER_OPTIONS, useValue: loggerConfig || {}},
+                {
+                    provide: LogService,
+                    useFactory: LogServiceFactory,
+                    deps: [LOGGER_LEVELS, LOGGER_CONSOLE, PrefixService, LOGGER_OPTIONS]
+                }
+            ]
+        };
     }
 }
